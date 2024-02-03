fc66d3136b5a778baa53eaf1db80d46ac20206bf
wadoodalam <wadoodalam@outlook.com>
2024-02-03 14:57:46 -0800

    Underscores cleaned for categorical features. Generate data profile for numerical features

--- data_profile.py
+++ data_profile.py
@@ -4,6 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# TODO: Need to handle outliers(and then missing values)
+
 def Graph(data):
     # Create histogram
     data.hist()
@@ -22,26 +24,33 @@ def PartitionDataset(data):
     data = data.head(10000)
     return data
 
-def Str_toNumeric(data, feature_names):
-    for feature_name in feature_names:
-        data[feature_name] = pd.to_numeric(data[feature_name].str.replace('_', ''), errors='coerce')
-   
+def RemoveUnderscore(data, feature_names,Sting=False):
+    if Sting:
+        for feature_name in feature_names:
+            data[feature_name] = data[feature_name].apply(lambda x: pd.NA if x.strip('_') == '' else x)
+    else:  
+        for feature_name in feature_names:
+            data[feature_name] = pd.to_numeric(data[feature_name].str.replace('_', ''), errors='coerce')
+    
     return data
 
 
 if __name__ == "__main__":
 
-    # missing values are represented by 99/-99 in the dataset
+    
     data = pd.read_csv('train.csv')
     dataset = PartitionDataset(data)
-    # TODO: Credit_Mix is str, _ should be na value, ,  Amount_invested_monthly null values are represented by 10000
-    dataset = Str_toNumeric(dataset,['Age','Num_of_Loan','Num_of_Delayed_Payment','Annual_Income','Changed_Credit_Limit','Outstanding_Debt','Amount_invested_monthly'])
-    dataset.loc[(dataset['Age'] < 18) | (dataset['Age'] > 100), 'Age'] = np.nan
+  
+    # underscores are cleaned
+    dataset = RemoveUnderscore(dataset,['Age','Num_of_Loan','Num_of_Delayed_Payment','Annual_Income','Changed_Credit_Limit','Outstanding_Debt','Amount_invested_monthly'])
+    dataset = RemoveUnderscore(dataset,['Occupation','Credit_Mix'],Sting=True)
+   
+    #dataset.loc[(dataset['Age'] < 18) | (dataset['Age'] > 100), 'Age'] = np.nan
 
     
     
     dataset.to_csv('10kData.csv')
-    #Describe(data) # mean, median, max, min, num_of_missing values
+    Describe(dataset) # mean, median, max, min, num_of_missing values
     
     #Graph(data) # Create Histogram
     
