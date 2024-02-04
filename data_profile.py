2373190fb92bbd60734fa8a05606c20707e676f9
wadoodalam <wadoodalam@outlook.com>
2024-02-03 18:28:09 -0800

    Plots and entire data description

--- data_profile.py
+++ data_profile.py
@@ -3,17 +3,39 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import re
 
 # TODO: Need to handle outliers(and then missing values)
 
-def Graph(data):
+def HistForNumData(data):
     # Create histogram
     data.hist()
     # Save the histogram to this file name
     plt.savefig('histogram.png')
+    
+def PlotForCatData(data):
+    feature_names = ['Credit_Score', 'Credit_Mix', 'Payment_Behaviour', 'Payment_of_Min_Amount']
+    for feature_name in feature_names:
+        plt.figure(figsize=(10, 6))
+        data[feature_name].value_counts().plot(kind='barh', color='black')
+        plt.title(feature_name)
+        plt.xlabel('Count of Each Class')
+        plt.ylabel('Class')
+        plt.tight_layout()
+        plt.savefig(f'{feature_name}.png')
 
+
+def DescribeCatData(data):
+    data_info = data[['Credit_Score', 'Credit_Mix', 'Type_of_Loan', 'Payment_Behaviour', 'Payment_of_Min_Amount']].astype('object').describe()
+    data_info.to_csv('Cat_Profile.csv')
+
+def ConvertCreditHistoryAge(data):
+    # Convert the age to decimal values
+    return data
+
+
+    
 def Describe(data):
-    data = data.head(10000)
     description = data.describe()
     # Separate the desired columns
     description = description.loc[['min', 'max', 'mean', '50%','count']]
@@ -27,6 +49,7 @@ def PartitionDataset(data):
 def RemoveUnderscore(data, feature_names,Sting=False):
     if Sting:
         for feature_name in feature_names:
+            # Lamda func that return the original element x if _ found. If only _ then put Null value
             data[feature_name] = data[feature_name].apply(lambda x: pd.NA if x.strip('_') == '' else x)
     else:  
         for feature_name in feature_names:
@@ -44,13 +67,13 @@ if __name__ == "__main__":
     # underscores are cleaned
     dataset = RemoveUnderscore(dataset,['Age','Num_of_Loan','Num_of_Delayed_Payment','Annual_Income','Changed_Credit_Limit','Outstanding_Debt','Amount_invested_monthly'])
     dataset = RemoveUnderscore(dataset,['Occupation','Credit_Mix'],Sting=True)
-   
+    #dataset = ConvertCreditHistoryAge(dataset)
     #dataset.loc[(dataset['Age'] < 18) | (dataset['Age'] > 100), 'Age'] = np.nan
 
     
     
     dataset.to_csv('10kData.csv')
-    Describe(dataset) # mean, median, max, min, num_of_missing values
+    PlotForCatData(dataset) # mean, median, max, min, num_of_missing values
     
     #Graph(data) # Create Histogram
     
