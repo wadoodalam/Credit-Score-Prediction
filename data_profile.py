747b4b00da8f4b28cb181fb94956b15b7561d0cf
wadoodalam <wadoodalam@outlook.com>
2024-03-17 06:41:33 -0700

    numeric feature recognistion bug fixed

--- data_profile.py
+++ data_profile.py
@@ -13,11 +13,14 @@ import re,warnings
 warnings.filterwarnings("ignore")
 
 def GraphMissingValues(data):
+    # find missing values and features
     missing_values = data.isnull().sum()
     missing_values = missing_values[missing_values > 0]
     total_missing_values = data.isnull().sum().sum()
-
+    # Print # of missing values
     print("Total number of missing values:", total_missing_values)
+    # print number of missing value features
+    print('Total number of missing value features',len(missing_values))
     # Plotting
     plt.figure(figsize=(10, 6))
     missing_values.plot(kind='bar', color='skyblue')
@@ -27,7 +30,8 @@ def GraphMissingValues(data):
     plt.xticks(rotation=45)
     plt.grid(axis='y', linestyle='--', alpha=0.7)
     plt.tight_layout()
-    plt.show()
+    # save the figure
+    plt.savefig('missing_values.png')
 
 def HistForNumData(data):
     # Create histogram
@@ -35,19 +39,8 @@ def HistForNumData(data):
     # Save the histogram to this file name
     plt.savefig('histogram.png')
     
-def PlotForCatData(data):
-    feature_names = ['Credit_Score', 'Credit_Mix', 'Payment_Behaviour', 'Payment_of_Min_Amount']
-    for feature_name in feature_names:
-        plt.figure(figsize=(10, 6))
-        data[feature_name].value_counts().plot(kind='barh', color='black')
-        plt.title(feature_name)
-        plt.xlabel('Count of Each Class')
-        plt.ylabel('Class')
-        plt.tight_layout()
-        plt.savefig(f'{feature_name}.png')
-
 def DescribeCatData(data):
-    data_info = data[['Credit_Score', 'Credit_Mix', 'Payment_Behaviour']].astype('object').describe()
+    data_info = data[['Credit_Mix', 'Payment_Behaviour','Occupation','Month']].astype('object').describe()
     data_info.to_csv('Cat_Profile.csv')
 
 def ConvertCreditHistoryAge(age_str):
@@ -138,23 +131,87 @@ def ConvertCreditScoreClass(data):
 
 def ConvertPaymentMinAmountClass(data):
     amount = data['Payment_of_Min_Amount']
-    class_map = {'No':0, 'Yes':1}
+    class_map = {'No': 0, 'Yes': 1}
     
     num_classes = []
     for classes in amount:
         if isinstance(classes, str):
-            num_val = class_map[classes]
+            classes = classes.strip()
+            if classes in class_map:
+                num_val = class_map[classes]
+            else:
+                num_val = np.nan
         else:
-            num_val = pd.NA
+            num_val = np.nan
         num_classes.append(num_val)
+    
+    # Drop the original column and replace it with the converted numeric column
     data = data.drop(columns=['Payment_of_Min_Amount'])
     data['Payment_of_Min_Amount'] = num_classes
+    
+
     return data
+
+def HistForCat(cat_feature,image_name):
+    # Count occurrences of each class
+    class_counts = cat_feature.value_counts()
+    # Plotting
+    plt.barh(range(len(class_counts)), class_counts.values, color='black')  # Horizontal bar chart with black color
+    plt.xlabel('Count of Occurrences')
+    plt.ylabel('Class')
+    plt.title('Histogram of ' + cat_feature.name)
+    plt.yticks(range(len(class_counts)), class_counts.index)
+    
+    # save the image of hist
+    plt.savefig(image_name)
+
+def PrintNumberOfNumericFeature(data):
+    numerical_features = []
+    
+    # Iterate through columns
+    for column in data.columns:
+        # check if numeric type then append to list
+        if pd.api.types.is_numeric_dtype(data[column]):
+            numerical_features.append(column)
+
+    # print the length of the list(count)-'Credit_Score'       
+    print("Number of numerical features:", len(numerical_features)-1)
+    
+def RemoveUnderscoreMonthlyBalance(string_value):
+    if pd.isna(string_value) or string_value == '':
+        return np.nan  # Return NaN for missing values
+    elif isinstance(string_value, float):
+        return string_value  # Return float values unchanged
+    else:
+        # Use regular expressions to extract numeric part
+        numeric_part = re.sub(r'[^0-9.-]', '', string_value)
+        # Convert extracted numeric part to float
+        numeric_value = float(numeric_part)
+        return numeric_value
+    
+def PlotForCatData(data):
+    feature_names = ['Credit_Mix', 'Payment_Behaviour','Occupation','Month']
+    for feature_name in feature_names:
+        plt.figure(figsize=(10, 6))
+        data[feature_name].value_counts().plot(kind='barh', color='black')
+        plt.title(feature_name)
+        plt.xlabel('Count of Each Class')
+        plt.ylabel('Class')
+        plt.tight_layout()
+        plt.savefig(f'{feature_name}.png')
+
+def RemoveUnderscoreMNumOfLoan(value):
+    if isinstance(value, int):  # Check if the value is already numeric
+        return value
+    else:
+        numeric_part = re.findall(r'\d+\.*\d*', str(value))[0]
+    return int(numeric_part)
+
 
 
 if __name__ == "__main__":
 
-    
+    # load the dataset    
     data = pd.read_csv('train.csv')
     # Drop unneeded columns
     data = data.drop(columns=['ID','Name','SSN'])
@@ -162,8 +219,11 @@ if __name__ == "__main__":
     dataset = PartitionDataset(data)
   
     # underscores are cleaned
-    dataset = RemoveUnderscore(dataset,['Age','Num_of_Loan','Num_of_Delayed_Payment','Annual_Income','Changed_Credit_Limit','Outstanding_Debt','Amount_invested_monthly'])
+    dataset = RemoveUnderscore(dataset,['Age','Num_of_Delayed_Payment','Annual_Income','Changed_Credit_Limit','Outstanding_Debt','Amount_invested_monthly'])
     dataset = RemoveUnderscore(dataset,['Occupation','Credit_Mix'],Sting=True)
+    dataset['Monthly_Balance'] = dataset['Monthly_Balance'].apply(RemoveUnderscoreMonthlyBalance)
+    dataset['Num_of_Loan'] = dataset['Num_of_Loan'].apply(RemoveUnderscoreMNumOfLoan)
+    
     # Age is converted to decimal for Credit History Age
     dataset['Credit_History_Age'] = dataset['Credit_History_Age'].apply(ConvertCreditHistoryAge)
     # Identify and replace null values
@@ -173,11 +233,28 @@ if __name__ == "__main__":
     
     dataset = ConvertCreditScoreClass(dataset)
     dataset = ConvertPaymentMinAmountClass(dataset)
+    
+    # Describe and generate csv tables for numeric and categorical features
     Describe(dataset)
     DescribeCatData(dataset)
-    #GraphMissingValues(dataset)
-    dataset.to_csv('10kData.csv')
-    #PlotForCatData(dataset) # mean, median, max, min, num_of_missing values
     
-    #Graph(data) # Create Histogram
+    # Graph missing values by feature
+    GraphMissingValues(dataset)
+    
+    # Print number of numerical feature
+    PrintNumberOfNumericFeature(dataset)
+    # Print types(and counts) of numeric features in the dataset
+    PlotForCatData(dataset)
+    
+   
+    data_info = dataset[['Num_of_Loan']].describe()
+    
+    
+
+    data_info.to_csv('missed_Profile.csv')
+    
+    # save the claned dataset to csv
+    dataset.to_csv('10kData.csv')
+    
+    
     
